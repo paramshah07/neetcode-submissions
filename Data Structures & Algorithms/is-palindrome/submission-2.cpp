3d6c5cc0232837c443e00262355d33b10e8d7a42
paramshah07 <107650333+paramshah07@users.noreply.github.com>
2026-08-06 16:29:29 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(const string& s) {
+        int l = 0, r = s.size() - 1;
+
+        while(l < r) {
+            // skips if not alnum
+            while(l<r && !isalnum(static_cast<unsigned char>(s[l]))) ++l;
+            while(l<r && !isalnum(static_cast<unsigned char>(s[r]))) --r;
+
+            if(tolower(static_cast<unsigned char>(s[l])) != tolower(static_cast<unsigned char>(s[r]))) return false;
+
+            ++l, --r;
+        }
+        return true;
+    }
+};
